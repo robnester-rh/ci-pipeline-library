07d86fe2dda0ab60089f2e2f52895e0b1ea8db47
Rob Nester <rnester@redhat.com>
2018-01-04 11:18:28 -0500

    ADD initial infra provision code.
    Added initial POC code for provisioning a host on a cloud provider.

---
+++ vars/infraProvision.groovy
@@ -0,0 +1,48 @@
+def call(Map config){
+    Integer count = config.containsKey('count') ? config.count : 1
+    String platformType = config.platformType ?: 'static'
+    String machineType = config.machineType ?: 'cloud'
+    String cloudProvider = null
+    if ( machineType == 'cloud' ){
+        cloudProvider = config.cloudProvider ?: 'aws'
+    }
+
+    if (platformType == 'static'){
+        if (machineType == 'cloud'){
+            if ( cloudProvider == 'aws'){
+                node(){
+                    stage("Deploy ${count} ${machineType} host(s) on ${cloudProvider}"){
+                        ansiColor('xterm') {
+                            ansibleTower credential: '',
+                                    extraVars: '''---
+                                    job: "${JOB_NAME}"
+                                    build: "${BUILD_NUMBER}"''',
+                                    importTowerLogs: true,
+                                    importWorkflowChildLogs: true,
+                                    inventory: '',
+                                    jobTags: '',
+                                    jobTemplate: 'Provision RHEL on AWS',
+                                    limit: '',
+                                    removeColor: false,
+                                    templateType: 'workflow',
+                                    towerServer: 'Local Tower',
+                                    verbose: true
+                        }
+                    }
+                }
+
+
+            }
+
+        } else if ( machineType == 'baremetal') {
+
+        } else {
+            print("Invalid machine type")
+        }
+
+    } else if (platformType == 'dynamic'){
+
+    } else {
+        print("Invalid platform type")
+    }
+}
