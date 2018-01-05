cf53412362dbb437634cae3bc2e8b875321c15c4
Rob Nester <rnester@redhat.com>
2018-01-05 15:12:59 -0500

    Move provisionCloud to it file

--- vars/infraProvision.groovy
+++ vars/infraProvision.groovy
@@ -21,31 +21,3 @@ def call(configYaml) {
 
     }
 }
-
-def provisionCloud(type, count) {
-    println("Provisioning ${count} host(s) on ${type}")
-    if (type.toLowerCase() == 'aws') {
-        node{
-            stage("Provisioning infra"){
-                ansiColor('xterm') {
-                    echo "Deploying ${machineType} host on ${cloudProvider}"
-                    ansibleTower credential: '',
-                            extraVars: '''---
-                                    job: "${JOB_NAME}"
-                                    build: "${BUILD_NUMBER}"
-                                    count: "${count}"''',
-                            importTowerLogs: true,
-                            importWorkflowChildLogs: true,
-                            inventory: '',
-                            jobTags: '',
-                            jobTemplate: 'Provision RHEL on AWS',
-                            limit: '',
-                            removeColor: false,
-                            templateType: 'workflow',
-                            towerServer: 'Local Tower',
-                            verbose: false
-                }
-            }
-        }
-    }
-}
---
+++ vars/provisionCloud.groovy
@@ -0,0 +1,27 @@
+def call(String type, String count) {
+    println("Provisioning ${count} host(s) on ${type}")
+    if (type.toLowerCase() == 'aws') {
+        node{
+            stage("Provisioning infra"){
+                ansiColor('xterm') {
+                    echo "Deploying ${machineType} host on ${cloudProvider}"
+                    ansibleTower credential: '',
+                            extraVars: '''---
+                                    job: "${JOB_NAME}"
+                                    build: "${BUILD_NUMBER}"
+                                    count: "${count}"''',
+                            importTowerLogs: true,
+                            importWorkflowChildLogs: true,
+                            inventory: '',
+                            jobTags: '',
+                            jobTemplate: 'Provision RHEL on AWS',
+                            limit: '',
+                            removeColor: false,
+                            templateType: 'workflow',
+                            towerServer: 'Local Tower',
+                            verbose: false
+                }
+            }
+        }
+    }
+}
