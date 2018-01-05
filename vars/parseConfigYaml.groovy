782d2228aeabe271f2eddb98d1fa9c9ff2c1aa16
Rob Nester <rnester@redhat.com>
2018-01-05 15:52:20 -0500

    REFACTOR for yaml parsing

--- vars/parseConfigYaml.groovy
+++ vars/parseConfigYaml.groovy
@@ -6,15 +6,15 @@ def call(String filename="config"){
     Yaml parser = new Yaml()
 
     try{
-        env.configYaml = parser.load(("${WORKSPACE}/${filename}.yaml" as File).text)
+        return parser.load(("${WORKSPACE}/${filename}.yaml" as File).text)
         echo "Found ${filename}.yaml"
 
 
     } catch ( FileNotFoundException ){
         try{
-            env.configYaml = parser.load(("${WORKSPACE}/${filename}.yml" as File).text)
+            return parser.load(("${WORKSPACE}/${filename}.yml" as File).text)
             echo "Found ${filename}.yml"
-            
+
         } catch ( FileNotFoundException e ){
             println(e)
             System.exit(1)
