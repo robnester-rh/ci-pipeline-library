ac1d31ab03893b666d480403fd98c659940d47d3
Rob Nester <rnester@redhat.com>
2018-01-05 14:03:35 -0500

    ADD extraVar for count

--- vars/infraProvision.groovy
+++ vars/infraProvision.groovy
@@ -22,26 +22,26 @@ def call(configYaml) {
 def provisionCloud(type, count) {
     println("Provisioning ${count} host(s) on ${type}")
     if (type.toLowerCase() == 'aws') {
-        for ( i in count.toInteger() ) {
-            node{
-                stage("Provisioning infra"){
-                    ansiColor('xterm') {
-                        echo "Deploying ${machineType} host on ${cloudProvider}"
-                        ansibleTower credential: '',
-                                extraVars: '''---
-                                        job: "${JOB_NAME}"
-                                        build: "${BUILD_NUMBER}"''',
-                                importTowerLogs: true,
-                                importWorkflowChildLogs: true,
-                                inventory: '',
-                                jobTags: '',
-                                jobTemplate: 'Provision RHEL on AWS',
-                                limit: '',
-                                removeColor: false,
-                                templateType: 'workflow',
-                                towerServer: 'Local Tower',
-                                verbose: false
-                    }
+
+        node{
+            stage("Provisioning infra"){
+                ansiColor('xterm') {
+                    echo "Deploying ${machineType} host on ${cloudProvider}"
+                    ansibleTower credential: '',
+                            extraVars: '''---
+                                    job: "${JOB_NAME}"
+                                    build: "${BUILD_NUMBER}"
+                                    count: "${count}"''',
+                            importTowerLogs: true,
+                            importWorkflowChildLogs: true,
+                            inventory: '',
+                            jobTags: '',
+                            jobTemplate: 'Provision RHEL on AWS',
+                            limit: '',
+                            removeColor: false,
+                            templateType: 'workflow',
+                            towerServer: 'Local Tower',
+                            verbose: false
                 }
             }
         }
