f56e4b7686c6ba39d6fde88eb16ba293139bf3fb
Rob Nester <rnester@redhat.com>
2018-01-05 15:34:54 -0500

    UPDATE infraProvision for debug

--- vars/infraProvision.groovy
+++ vars/infraProvision.groovy
@@ -1,13 +1,16 @@
 def call() {
 
     configYaml = env.configYaml
+    echo "From infraProvision"
+    echo configYaml.getClass().toString()
+    echo configYaml
 
     def cloudConfig = configYaml['infra']['cloud'] ?: null
     def baremetalConfig = configYaml['infra']['baremetal'] ?: null
     def containerConfig = configYaml['infra']['container'] ?: null
 
     if (cloudConfig) {
-        echo cloudConfig.toString()
+        echo cloudConfig
         provisionCloud(cloudConfig[0]['type'], cloudConfig[0]['count'])
 
 //        cloudConfig.each { provisionCloud(it.type, it.count) }
