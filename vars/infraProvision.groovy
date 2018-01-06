2061bee43204362de6c5b1f4ac03a6e4c13ddf82
Rob Nester <rnester@redhat.com>
2018-01-05 21:10:20 -0500

    DEBUG

--- vars/infraProvision.groovy
+++ vars/infraProvision.groovy
@@ -4,13 +4,13 @@ def call() {
     echo env.configYaml.getClass().toString()
 
 
-    def cloudConfig = env.configYaml['infra']['cloud'] ?: null
-    def baremetalConfig = env.configYaml['infra']['baremetal'] ?: null
-    def containerConfig = env.configYaml['infra']['container'] ?: null
-
-    if (cloudConfig) {
-        echo cloudConfig
-        provisionCloud(cloudConfig[0]['type'], cloudConfig[0]['count'])
+//    def cloudConfig = env.configYaml['infra']['cloud'] ?: null
+//    def baremetalConfig = env.configYaml['infra']['baremetal'] ?: null
+//    def containerConfig = env.configYaml['infra']['container'] ?: null
+//
+//    if (cloudConfig) {
+//        echo cloudConfig
+//        provisionCloud(cloudConfig[0]['type'], cloudConfig[0]['count'])
 
 //        cloudConfig.each { provisionCloud(it.type, it.count) }
 //        for ( config in cloudConfig ) {
@@ -18,13 +18,13 @@ def call() {
 //                provisionCloud(config.type, config.count)
 //            }
 //        }
-    }
-
-    if (baremetalConfig) {
-
-    }
-
-    if (containerConfig) {
-
-    }
+//    }
+//
+//    if (baremetalConfig) {
+//
+//    }
+//
+//    if (containerConfig) {
+//
+//    }
 }
